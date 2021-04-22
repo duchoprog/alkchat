25778266849883d7b91c7be47eaf7d3520e5052a
pabloduchovny <pablo.duchovny@gmail.com>
2021-04-22 09:57:54 -0300

    Initial commit

---
+++ src/main/java/com/ducho/alkchat/AlkchatApplication.java
@@ -0,0 +1,13 @@
+package com.ducho.alkchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ducho/alkchat/AlkchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducho.alkchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
