c7e5ae030f3d61ca27bbbe4bdd7cb6ae74bfeb71
pabloduchovny <pablo.duchovny@gmail.com>
2021-04-23 07:29:11 -0300

    Added mapping and regiter.html for registration

--- src/main/java/com/ducho/alkchat/controllers/AppController.java
+++ src/main/java/com/ducho/alkchat/controllers/AppController.java
@@ -1,14 +1,21 @@
 package com.ducho.alkchat.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ducho.alkchat.User;
 
 @Controller
 public class AppController {
 	@GetMapping("")
 	public String viewHome(){
 		return "index";
-		
+	}
+	@GetMapping("/register")
+	public String viewRegister(Model model){
+		model.addAttribute("user", new User());
+		return "register";
 	}
 
 }
