db7217dcfc1ef18d857d93d58e62e417e258dc91
pabloduchovny <pablo.duchovny@gmail.com>
2021-04-24 08:33:39 -0300

    Added Registration form

--- src/main/java/com/ducho/alkchat/controllers/AppController.java
+++ src/main/java/com/ducho/alkchat/controllers/AppController.java
@@ -1,13 +1,18 @@
 package com.ducho.alkchat.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ducho.alkchat.User;
+import com.ducho.alkchat.UserRepository;
 
 @Controller
 public class AppController {
+	@Autowired
+	private UserRepository userRepo;
 	@GetMapping("")
 	public String viewHome(){
 		return "index";
@@ -17,5 +22,21 @@ public class AppController {
 		model.addAttribute("user", new User());
 		return "register";
 	}
+	
+	/*
+	 * @PostMapping("/process_reg") public String processReg(User user) {
+	 * userRepo.save(user); return "culo";}
+	 */
+@PostMapping("/process_reg")
+public String processRegister(User user) {
+	
+	userRepo.save(user);
+	
+	return "reg_success";
+}
+
+
 
 }
+
+
