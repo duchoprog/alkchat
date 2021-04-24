972eacb6d61b00934f0745105e1f8dfcefc9da41
pabloduchovny <pablo.duchovny@gmail.com>
2021-04-24 09:12:53 -0300

    Implemented user details and user details service

---
+++ src/main/java/com/ducho/alkchat/services/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.ducho.alkchat.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.ducho.alkchat.CustomUserDetails;
+import com.ducho.alkchat.User;
+import com.ducho.alkchat.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+		User user=repo.findByEmail(email);
+		if (user==null) {
+			throw new UsernameNotFoundException("Usuario no esiste");
+		}
+		return new CustomUserDetails(user);
+	}
+
+}
